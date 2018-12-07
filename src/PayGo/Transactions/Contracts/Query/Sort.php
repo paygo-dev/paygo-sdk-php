757b73e2c1a8c12ead76bfb95eabd5c1bb6b4513
Adriano M. La Selva <a.moreira@paygo.com.br>
2018-12-07 15:56:37 -0200

    Correção de serialização de objeto Sort

--- src/PayGo/Transactions/Contracts/Query/Sort.php
+++ src/PayGo/Transactions/Contracts/Query/Sort.php
@@ -21,6 +21,42 @@ class Sort implements \JsonSerializable
     private $mode = SortModeConst::ASC;
 
     /**
+     * @return string
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param string $field
+     * @return Sort
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMode()
+    {
+        return $this->mode;
+    }
+
+    /**
+     * @param string $mode
+     * @return Sort
+     */
+    public function setMode($mode)
+    {
+        $this->mode = $mode;
+        return $this;
+    }
+
+    /**
      * @return array|mixed
      */
     public function jsonSerialize()
